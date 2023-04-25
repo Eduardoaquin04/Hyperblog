3151d43633948520a087f8363bc92a3e0ca65132
eduardo aquin <aquin.j.eduardo@gmail.com>
2023-04-25 00:03:01 -0400

    Otro commit mas

--- elemploPython.py
+++ elemploPython.py
@@ -1,1 +1,7 @@
+
+
 print("Hola desde Vscode")
+precio = input("Ingrese el precio del producto:")
+cantidad = input("Ingrese la cantidad a comprar:")
+total = int(precio) * int(cantidad)
+print("El total a pagar es de {}".format(total))
